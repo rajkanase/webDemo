092c2d1989b6e74d1a3ec7617590544162a1c763
Rajkumar Kanase <rajkanase11@gmail.com>
2017-10-25 17:23:06 +0530

    commit b4 format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { BgVideoComponent } from './bg-video/bg-video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProductsComponent,
+    WorkComponent,
+    ContactComponent,
+    FooterComponent,
+    BgVideoComponent
   ],
   imports: [
     BrowserModule
